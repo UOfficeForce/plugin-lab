581a9f72118492e5d8565ae0585b2fe6ef5507cb
holly <holly@edetw.com>
2023-08-03 12:24:28 +0800

    初始化

---
+++ Ede.Uofx.Customize.Web/Controllers/CustomersController.cs
@@ -0,0 +1,153 @@
+﻿using Ede.Uofx.Customize.Web.Models;
+using Ede.Uofx.Customize.Web.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+
+namespace Ede.Uofx.Customize.Web.Controllers
+{
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet("CheckedPrice")]
+        public string CheckedPrice(string? price)
+        {
+
+            if (price == null)
+                return "";
+
+            if (Convert.ToDecimal(price) > 10)
+            {
+                return "金額不可超過10";
+            }
+
+            return "";
+        }
+
+        [HttpPost("CheckedOrderPrice")]
+        public string CheckedOrderPrice(CheckedOrderPriceModel model)
+        {
+
+            if (model.price == null)
+                return "";
+
+            if (Convert.ToDouble(model.price) > Convert.ToDouble(model.ProductPrice) * 0.8)
+            {
+                return "實際金額不可大於產品金額8成";
+            }
+
+            return "";
+        }
+
+        [HttpPost("CreateFormData")]
+        public string CreateFormData(FormData model)
+        {
+
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.CreateOrder(trans, model.FORM_NBR, model.CATEGORY_ID, model.PRODUCT_ID,
+                    model.APPLICANT,model.UNIT_PRICE.ToString(), model.FORM_RESULT);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+
+            return "";
+        }
+
+        [HttpPost("UpdateFormData")]
+        public string UpdateFormData(FormStatus model)
+        {
+
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.UpdateOrder(trans, model.FORM_NBR, model.FORM_RESULT);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+
+            return "";
+        }
+
+
+
+        [HttpGet("GetCategories")]
+        public List<Categories> GetCategories()
+        {
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.GetCategories(trans);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+        }
+
+        [HttpGet("GetProducts")]
+        public List<Products> Products(string? categoryID)
+        {
+            if (string.IsNullOrEmpty(categoryID))
+            {
+                return new List<Products>();
+            }
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.GetProducts(trans, categoryID);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+        }
+
+        [HttpGet("GetProductPrice")]
+        public List<Products> GetProductPrice(string? productID)
+        {
+            if (string.IsNullOrEmpty(productID))
+            {
+                return new List<Products>();
+            }
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.GetProductPrice(trans, productID);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+        }
+
+        [HttpGet("GetCustomers")]
+        public List<Customer> GetCustomers()
+        {
+            SqlTransaction trans = Connection.GetTransaction();
+            try
+            {
+                Connection.OpenConnection(trans);
+                return CustomersService.GetCustomers(trans);
+            }
+            finally
+            {
+                Connection.CloseConnection(trans);
+            }
+        }
+
+
+    }
+}
---
+++ Ede.Uofx.Customize.Web/Models/Unit.cs
@@ -0,0 +1,59 @@
+﻿namespace Ede.Uofx.Customize.Web.Models
+{
+    public class Customers
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+
+
+    }
+
+
+    public class FormStatus
+    {
+        public string FORM_NBR { get; set; }
+        public string FORM_RESULT { get; set; }
+    }
+
+    public class FormData
+    {
+        public string FORM_NBR { get; set; }
+        public string CATEGORY_ID { get; set; }
+        public string PRODUCT_ID { get; set; }
+        public string APPLICANT { get; set; }
+        public decimal UNIT_PRICE { get; set; }
+        public string FORM_RESULT { get; set; }
+    }
+
+
+    public class CheckedOrderPriceModel
+    { 
+        public decimal? price { get; set; }
+        public decimal? ProductPrice { get; set; }
+    }
+
+
+    public class Categories
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+
+    public class Products
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string UnitPrice { get; set; }
+
+    }
+
+    public class Customer
+    {
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+
+}
