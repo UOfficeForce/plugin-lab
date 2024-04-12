a8dc0488610925ae482e378ad608237c90e92cce
woodman <woodman@edetw.com>
2024-04-12 16:17:32 +0800

    修改app模式預設路徑

--- Ede.Uofx.Customize.Web/ClientApp/webpack-exposes.config.js
+++ Ede.Uofx.Customize.Web/ClientApp/webpack-exposes.config.js
@@ -7,7 +7,7 @@ const exposes = {
     //勿刪除存放Web欄位的路徑
   },
   app: {
-    // './TemplateField': './src/app/web/template-field/template-field.module.ts'
+     './TemplateField': './src/app/mobile/template-field/template-field.module.ts'
     //勿刪除存放App欄位的路徑
   }
 };
