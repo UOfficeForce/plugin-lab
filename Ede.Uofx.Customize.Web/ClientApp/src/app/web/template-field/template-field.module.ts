7a5eacef7891359db56fba936e140780a05df110
woodman <woodman@edetw.com>
2024-04-12 16:19:19 +0800

    整理常用的元件

--- Ede.Uofx.Customize.Web/ClientApp/src/app/web/template-field/template-field.module.ts
+++ Ede.Uofx.Customize.Web/ClientApp/src/app/web/template-field/template-field.module.ts
@@ -1,11 +1,3 @@
-/*
-此為外掛欄位module的樣板，修改/置換的項事如下
-修改import 各模式的Component
-修改const COMPONENTS  各模式的Component
-修改NgModule中的 RouterModule  各模式的Component
-修改 class name 及各模式的module
-*/
-
 import {
   BASIC_HTTP_HANDLER,
   BasicHttpHandler,
@@ -15,28 +7,47 @@ import {
   UofxFormFieldBaseModule,
   UofxFormModule,
 } from '@uofx/web-components/form';
+import { UofxLoadingModule, UofxTranslateModule } from '@uofx/web-components';
 
 import { BasicHttpClient } from '@service/basic-http-client';
+import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { CheckboxModule } from 'primeng/checkbox';
 import { CommonModule } from '@angular/common';
+import { CustomerService } from '@service/gm.service';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SelectItemComponent } from './select-item/select-item.component';
+import { TableModule } from 'primeng/table';
 import { TemplateFieldPropsComponent } from './props/template-field.props.component';
 import { TemplateFieldWriteComponent } from './write/template-field.write.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { UofxButtonModule } from '@uofx/web-components/button';
 import { UofxDialogModule } from '@uofx/web-components/dialog';
 import { UofxIconModule } from '@uofx/web-components/icon';
 import { UofxPluginApiService } from '@uofx/plugin-api';
+import { UofxTextareaModule } from '@uofx/web-components/textarea';
 import { UofxToastModule } from '@uofx/web-components/toast';
-import { UofxTranslateModule } from '@uofx/web-components';
 import { UofxUserSelectModule } from '@uofx/web-components/user-select';
+
+/*
+此為外掛欄位module的樣板，修改/置換的項事如下
+修改import 各模式的Component
+修改const COMPONENTS  各模式的Component
+修改NgModule中的 RouterModule  各模式的Component
+修改 class name 及各模式的module
+*/
 
 /*↑↑↑↑修改import 各模式的Component↑↑↑↑*/
 
 const PRIMENG_MODULES = [
   CheckboxModule,
-  CalendarModule
+  CalendarModule,
+  TableModule,DialogModule,InputNumberModule,
+  DropdownModule,ButtonModule
 ];
 
 const UOF_MODULES = [
@@ -44,11 +55,13 @@ const UOF_MODULES = [
   UofxFormFieldBaseModule,
   UofxFormModule,
   UofxIconModule,
-
+UofxLoadingModule,
   UofxToastModule,
 
   UofxTranslateModule,
-  UofxUserSelectModule
+  UofxUserSelectModule,
+  UofxTextareaModule,
+  UofxButtonModule
 ];
 
 /*修改*/
@@ -86,9 +99,9 @@ const BASIC_SERVICES = [
     ...PRIMENG_MODULES,
     ...UOF_MODULES,
   ],
-  providers: [BASIC_SERVICES, UofxPluginApiService],
+  providers: [BASIC_SERVICES, UofxPluginApiService, CustomerService],
   exports: [...COMPONENTS],
-  declarations: [...COMPONENTS],
+  declarations: [...COMPONENTS, SelectItemComponent],
 })
 
 /*修改*/
